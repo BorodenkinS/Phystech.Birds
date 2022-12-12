1f44b7e11c3848de7a674ca17e004145da0a98f5
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-13 00:38:31 +0300

    Add files via upload
    Пока что только два типа свиней, остальные аналогичны, ибо нет доп. методов

---
+++ pigs.py
@@ -0,0 +1,47 @@
+import pymunk as pm
+import pygame as pg
+import math
+
+
+class Pig():
+    body = None
+    shape = None
+    image = None
+
+    def __init__(self, x, y, space):
+        self.body.position = pm.Vec2d(x, y)
+        space.add(self.body, self.shape)
+
+    def draw(self, screen):
+        angle_degrees = math.degrees(self.body.angle)
+        self.image = pg.transform.rotate(self.image, angle_degrees)
+        screen.blit(self.image, self.body.position)
+
+    def remove(self, space):
+        space.remove(self.body, self.shape)
+
+
+class DefaultPig(Pig):
+    mass = 5
+    life = 20
+    radius = 14
+    moment = pm.moment_for_circle(mass, 0, radius)
+    body = pm.Body(mass, moment)
+    shape = pm.Circle(body, radius, (0, 0))
+    shape.elasticity = 0.95
+    shape.friction = 1
+    shape.collision_type = 0
+    image = pg.image.load("defaultpig.png").convert_alpha()
+
+
+class KingPig(Pig):
+    mass = 10
+    life = 40
+    radius = 30
+    moment = pm.moment_for_circle(mass, 0, radius)
+    body = pm.Body(mass, moment)
+    shape = pm.Circle(body, radius, (0, 0))
+    shape.elasticity = 0.4
+    shape.friction = 2
+    shape.collision_type = 0
+    image = pg.image.load("kingpig.png").convert_alpha()
