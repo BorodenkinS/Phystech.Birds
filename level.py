860631b107648b8ce1cde618a9c867e2e413b8b7
Stepan <iqsborodenkin@gmail.com>
2022-12-13 11:53:44 +0300

    Level base

--- level.py
+++ level.py
@@ -1,1 +1,31 @@
-# Здесь игровой уровень
+import pymunk as pm
+import pygame as pg
+from characters import *
+from obstructions import *
+
+
+class Level:
+    def __init__(self, screen):
+        self.screen = screen
+        self.birds = []
+        self.pigs = []
+        self.obstructions = []
+        self.levels = [self.level1, self.level2, self.level3, self.level4, self.level5]
+
+    def __getitem__(self, item):
+        return self.levels[item]()
+
+    def level1(self):
+        pass
+
+    def level2(self):
+        pass
+
+    def level3(self):
+        pass
+
+    def level4(self):
+        pass
+
+    def level5(self):
+        pass
