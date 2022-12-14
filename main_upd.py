ed0f75625be8302e6d59410f253dccaf0cbe18c5
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-14 15:45:54 +0300

    Update main_upd.py

--- main_upd.py
+++ main_upd.py
@@ -1,64 +1,51 @@
-import pygame as pg
-import pymunk as pm
-import pymunk.pygame_util
-import birds
+# Поменять в main
+'''
+1) HEIGHT = 600
+2) Вставить в gameloop:
 
-pg.init()
-clock = pg.time.Clock()
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-WIDTH, HEIGHT = 1200, 600
-
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-space = pm.Space()
-pm.pygame_util.positive_y_is_up = False
-
-
-class Game:
-    FPS = 60
-    finished = False
-    level_state = True
-    sling1 = pm.Vec2d(135, 412)
-    sling2 = pm.Vec2d(160, 412)
-    number_of_birds = 1
-    birds_list = [birds.RedBird(700, 700, space, screen)]
-    rope_length = 100
-    slings_end = None
-    mouse_is_up = True
-
-    def main(self):
-        while not self.finished:
-            clock.tick(self.FPS)
-            space.step(1 / self.FPS)
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.finished = True
-                if self.level_state:
+- в цикл обработки событий:
+                    if self.level_state: (здесь параметр, определяющий, что запущен режим "уровень"
                     if (self.mouse_pressed() and self.number_of_birds > 0) or not self.mouse_is_up:
-                        self.prepare_to_fire2()
+                        self.prepare_to_fire()
                     else:
-                        self.slings_end = None
-
+                        self.slings_ends = []
+- в основной цикл:
+           -добавить сверху:
+               clock.tick(self.FPS)
+               space.step(1 / self.FPS)
+           -добавить после обработки событий:
             screen.fill(WHITE)
             if self.level_state:
                 for bird in self.birds_list:
-                    bird.draw()
+                    bird.draw(screen)
                 if not self.slings_end is None:
-                    pg.draw.line(screen, (0, 0, 0), (self.slings_end), (self.sling1), 5)
-                    pg.draw.line(screen, (0, 0, 0), (self.slings_end), (self.sling2), 5)
+                    pg.draw.line(screen, (0, 0, 0), self.slings_end, self.sling1, 5)
+                    pg.draw.line(screen, (0, 0, 0), self.slings_end, self.sling2, 5)
             pg.display.update()
 
+Что должно быть прописано в уровне:
+    sling1 = pm.Vec2d(135, 412)
+    sling2 = pm.Vec2d(160, 412)
+    number_of_birds = ...
+    birds_list = [birds.RedBird(...,..., screen, space)]
+    rope_length = 100
+    slings_end = None
+У меня это используется как атрибуты в Game
+
+Что должно быть добавлено в game:
+self.mouse_is_up = True
+
+Методы, которые добавляются целиком:
     def mouse_pressed(self):
-        '''ЛКМ нажата в нужном месте экрана'''
         x_mouse, y_mouse = pg.mouse.get_pos()
         cond1 = x_mouse > 80
-        cond2 = x_mouse < 250
-        cond3 = y_mouse > 262
+        cond2 = x_mouse < 262
+        cond3 = y_mouse > 350
         cond4 = y_mouse < 562
         cond5 = pg.mouse.get_pressed()[0]
         return cond1 * cond2 * cond3 * cond4 * cond5
 
-    def prepare_to_fire2(self):
+    def prepare_to_fire(self):
         if self.mouse_is_up:
             self.mouse_is_up = False
             self.number_of_birds -= 1
@@ -71,8 +58,4 @@ class Game:
         bird = self.birds_list[self.number_of_birds]
         bird.body.position = bird_position
         self.slings_end = bird_position - unit * bird.size
-
-
-g = Game()
-g.main()
-pg.quit()
+'''
