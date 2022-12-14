87a095bea6131eae301455a33c8107ce86cabdc3
Stepan <iqsborodenkin@gmail.com>
2022-12-14 18:47:15 +0300

    Рогатка

--- obstructions.py
+++ obstructions.py
@@ -1,1 +1,14 @@
-# Здесь балки всякие и тп
+import pygame
+import pymunk
+
+
+class Sling:
+
+    image = None
+
+    def __init__(self, screen):
+        self.sc = screen
+        self.sling_1 = pymunk.Vec2d(135, 412)
+        self.sling_2 = pymunk.Vec2d(160, 412)
+        self.sling_length = 100
+        self.sling_end = (self.sling_1 + self.sling_2)/2
