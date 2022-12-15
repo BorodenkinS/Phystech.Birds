0aab3929cfe9b3693529d24493f6c4ca05216556
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-16 00:46:10 +0300

    Функция прорисовки

--- obstructions.py
+++ obstructions.py
@@ -1,14 +1,20 @@
-import pygame
-import pymunk
+import pygame as pg
+import pymunk as pm
 
 
 class Sling:
 
     image = None
+    position = (0,0)
 
     def __init__(self, screen):
-        self.sc = screen
-        self.sling_1 = pymunk.Vec2d(135, 412)
-        self.sling_2 = pymunk.Vec2d(160, 412)
+        self.screen = screen
+        self.sling_1 = pm.Vec2d(135, 412)
+        self.sling_2 = pm.Vec2d(160, 412)
         self.sling_length = 100
         self.sling_end = (self.sling_1 + self.sling_2)/2
+
+    def draw(self):
+        self.screen.blit(self.image, self.position)
+        pg.draw.line(self.screen, (0, 0, 0), self.sling_end, self.sling_1, 5)
+        pg.draw.line(self.screen, (0, 0, 0), self.sling_end, self.sling_2, 5)
