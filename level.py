e83097ae84f75738384cb00f1b7078ff59e38db1
Andrew2612 <112695430+Andrew2612@users.noreply.github.com>
2022-12-17 15:21:32 +0300

    Update level.py

--- level.py
+++ level.py
@@ -33,50 +33,191 @@ class Level:
         # проработать механизм удаления прошлых объектов.
         # просто заносить в список на удаление, наверное, недостаточно, ибо функция удаления сработает позже
         # создания новых объектов
-        woodbeam = WoodBeam(905, 750, False, self.space, self.sc)
-        self.obstructions.append(woodbeam)
-        woodbeam = WoodBeam(995, 750, False, self.space, self.sc)
-        self.obstructions.append(woodbeam)
-        woodbeam = WoodBeam(950, 695, True, self.space, self.sc)
-        self.obstructions.append(woodbeam)
-        woodbeam = WoodBeam(905, 640, False, self.space, self.sc)
-        self.obstructions.append(woodbeam)
-        woodbeam = WoodBeam(995, 640, False, self.space, self.sc)
-        self.obstructions.append(woodbeam)
-        woodbeam = WoodBeam(950, 535, True, self.space, self.sc)
-        self.obstructions.append(woodbeam)
-        glassbeam = GlassBeam(895, 750, False, self.space, self.sc)
-        self.obstructions.append(glassbeam)
-        glassbeam = GlassBeam(800, 750, False, self.space, self.sc)
-        self.obstructions.append(glassbeam)
-        glassbeam = GlassBeam(845, 695, True, self.space, self.sc)
-        self.obstructions.append(glassbeam)
+        background_surf = pygame.image.load('Sprites\\bg 1 1200x600.png')
+        ground_surf = pygame.image.load('Sprites\\grass.png')
+        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200,600)))
+        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
 
-        pig = DefaultPig(950, 675, self.space, self.sc)
-        self.pigs.append(pig)
-        pig = DefaultPig(845, 785, self.space, self.sc)
-        self.pigs.append(pig)
-        pig = KingPig(950, 785, self.space, self.sc)
-        self.pigs.append(pig)
+        self.obstructions = [WoodBeam(1050, 475, False, self.space, self.sc),
+                             WoodBeam(1000, 475, False, self.space, self.sc),
+                             WoodBeam(1025, 420, True, self.space, self.sc),
+                             WoodBeam(920, 475, False, self.space, self.sc),
+                             WoodBeam(870, 475, False, self.space, self.sc),
+                             WoodBeam(895, 420, True, self.space, self.sc),
+                             WoodBeam(850, 475, False, self.space, self.sc),
+                             WoodBeam(800, 475, False, self.space, self.sc),
+                             WoodBeam(825, 420, True, self.space, self.sc)]
+        for beam in self.obstructions:
+            beam.draw()
 
-        bird = RedBird(100, 785, self.space, self.sc)
-        self.birds.append(bird)
-        bird = RedBird(75, 785, self.space, self.sc)
-        self.birds.append(bird)
-        bird = RedBird(50, 785, self.space, self.sc)
-        self.birds.append(bird)
-        self.number_of_birds = 3
+        self.pigs = [DefaultPig(1025, 400, self.space, self.sc),
+                     DefaultPig(895, 400, self.space, self.sc),
+                     DefaultPig(825, 400, self.space, self.sc)]
+        for pig in self.pigs:
+            pig.draw()
 
-        # beams ...
+        self.birds = [RedBird(100, 510, self.space, self.sc),
+                      RedBird(75, 510, self.space, self.sc),
+                      RedBird(50, 510, self.space, self.sc),
+                      RedBird(25, 510, self.space, self.sc)]
+        for bird in self.birds:
+            bird.draw()
+
 
     def level2(self):
-        pass
+        background_surf = pygame.image.load('Sprites\\bg 2 1200x600.png')
+        ground_surf = pygame.image.load('Sprites\\grass.png')
+        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
+        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+
+        self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
+                             WoodBeam(995, 475, False, self.space, self.sc),
+                             WoodBeam(950, 420, True, self.space, self.sc),
+                             WoodBeam(905, 365, False, self.space, self.sc),
+                             WoodBeam(995, 365, False, self.space, self.sc),
+                             WoodBeam(950, 260, True, self.space, self.sc),
+                             GlassBeam(895, 475, False, self.space, self.sc),
+                             GlassBeam(800, 475, False, self.space, self.sc),
+                             GlassBeam(845, 420, True, self.space, self.sc)]
+        for beam in self.obstructions:
+            beam.draw()
+
+        self.pigs = [DefaultPig(950, 400, self.space, self.sc),
+                     DefaultPig(845, 510, self.space, self.sc),
+                     KingPig(950, 495, self.space, self.sc)]
+        for pig in self.pigs:
+            pig.draw()
+
+        self.birds = [TriangleBird(100, 511, self.space, self.sc),
+                      TriangleBird(75, 511, self.space, self.sc),
+                      RedBird(25, 510, self.space, self.sc),
+                      RedBird(25, 510, self.space, self.sc)]
+        for bird in self.birds:
+            bird.draw()
 
     def level3(self):
-        pass
+        background_surf = pygame.image.load('Sprites\\bg 3 1200x600.png')
+        ground_surf = pygame.image.load('Sprites\\grass.png')
+        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
+        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+
+        self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
+                             WoodBeam(995, 475, False, self.space, self.sc),
+                             GlassBeam(950, 420, True, self.space, self.sc),
+                             WoodBeam(895, 475, False, self.space, self.sc),
+                             WoodBeam(800, 475, False, self.space, self.sc),
+                             GlassBeam(845, 420, True, self.space, self.sc),
+                             WoodBeam(1010, 475, False, self.space, self.sc),
+                             WoodBeam(1100, 475, False, self.space, self.sc),
+                             GlassBeam(1050, 420, True, self.space, self.sc),
+                             WoodBeam(960, 365, False, self.space, self.sc),
+                             WoodBeam(1050, 365, False, self.space, self.sc),
+                             GlassBeam(1000, 310, True, self.space, self.sc),
+                             WoodBeam(850, 365, False, self.space, self.sc),
+                             WoodBeam(940, 365, False, self.space, self.sc),
+                             GlassBeam(890, 310, True, self.space, self.sc),
+                             WoodBeam(910, 255, False, self.space, self.sc),
+                             WoodBeam(1000, 255, False, self.space, self.sc),
+                             GlassBeam(950, 200, True, self.space, self.sc),
+                             ]
+        for beam in self.obstructions:
+            beam.draw()
+
+        self.pigs = [DefaultPig(845, 510, self.space, self.sc),
+                     DeafultPig(950, 510, self.space, self.sc),
+                     DefaultPig(1050, 510, self.space, self.sc),
+                     DefaultPig(1000, 400, self.space, self.sc),
+                     DefaultPig(890, 400, self.space, self.sc),
+                     DefaultPig(950, 290, self.space, self.sc)]
+        for pig in self.pigs:
+            pig.draw()
+
+        self.birds = [BigBird(100, 495, self.space, self.sc),
+                      BigBird(65, 495, self.space, self.sc),
+                      RedBird(20, 510, self.space, self.sc)]
+        for bird in self.birds:
+            bird.draw()
 
     def level4(self):
-        pass
+        background_surf = pygame.image.load('Sprites\\bg 4 1200x600.png')
+        ground_surf = pygame.image.load('Sprites\\grass.png')
+        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
+        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+
+        self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
+                             WoodBeam(995, 475, False, self.space, self.sc),
+                             WoodBeam(950, 420, True, self.space, self.sc),
+                             GlassBeam(980, 475, False, self.space, self.sc),
+                             GalssBeam(920, 475, False, self.space, self.sc),
+                             WoodBeam(905, 365, False, self.space, self.sc),
+                             WoodBeam(995, 365, False, self.space, self.sc),
+                             WoodBeam(950, 260, True, self.space, self.sc),
+                             GlassBeam(980, 365, False, self.space, self.sc),
+                             GalssBeam(920, 365, False, self.space, self.sc),
+                             WoodBeam(1015, 475, False, self.space, self.sc),
+                             WoodBeam(110, 475, False, self.space, self.sc),
+                             WoodBeam(1065, 420, True, self.space, self.sc),
+                             GlassBeam(1095, 475, False, self.space, self.sc),
+                             GalssBeam(1035, 475, False, self.space, self.sc),
+                             WoodBeam(1020, 365, False, self.space, self.sc),
+                             WoodBeam(1110, 365, False, self.space, self.sc),
+                             WoodBeam(1065, 260, True, self.space, self.sc),
+                             GlassBeam(1095, 365, False, self.space, self.sc),
+                             GalssBeam(1035, 365, False, self.space, self.sc),
+                             ]
+        for beam in self.obstructions:
+            beam.draw()
+
+        self.pigs = [DefaultPig(950, 400, self.space, self.sc),
+                     DefaultPig(950, 510, self.space, self.sc),
+                     DefaultPig(1065, 400, self.space, self.sc),
+                     DefaultPig(1065, 510, self.space, self.sc)]
+
+
+        for pig in self.pigs:
+            pig.draw()
+
+        self.birds = [BigBird(100, 495, self.space, self.sc),
+                      BigBird(65, 495, self.space, self.sc),
+                      TriangleBird(20, 510, self.space, self.sc)]
+        for bird in self.birds:
+            bird.draw()
 
     def level5(self):
-        pass
+        background_surf = pygame.image.load('Sprites\\bg 5 1200x600.png')
+        ground_surf = pygame.image.load('Sprites\\grass.png')
+        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
+        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+
+        self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
+                             WoodBeam(995, 475, False, self.space, self.sc),
+                             WoodBeam(950, 420, True, self.space, self.sc),
+                             GlassBeam(980, 475, False, self.space, self.sc),
+                             GalssBeam(920, 475, False, self.space, self.sc),
+                             WoodBeam(905, 365, False, self.space, self.sc),
+                             WoodBeam(995, 365, False, self.space, self.sc),
+                             WoodBeam(950, 260, True, self.space, self.sc),
+                             GlassBeam(980, 365, False, self.space, self.sc),
+                             GalssBeam(920, 365, False, self.space, self.sc),
+                             WoodBeam(910, 255, False, self.space, self.sc),
+                             WoodBeam(1000, 255, False, self.space, self.sc),
+                             GlassBeam(950, 200, True, self.space, self.sc),
+                             ]
+        for beam in self.obstructions:
+            beam.draw()
+
+        self.pigs = [DefaultPig(950, 400, self.space, self.sc),
+                     DefaultPig(950, 510, self.space, self.sc),
+                     DefaultPig(950, 290, self.space, self.sc),
+                     DefaultPig(1050, 510, self.space, self.sc),
+                     DefaultPig(1090, 510, self.space, self.sc),
+                     KingPig(1120, 495, self.space, self.sc)]
+
+        for pig in self.pigs:
+            pig.draw()
+
+        self.birds = [BigBird(100, 495, self.space, self.sc),
+                      TriangleBird(60, 510, self.space, self.sc),
+                      RedBird(20, 510, self.space, self.sc)]
+        for bird in self.birds:
+            bird.draw()
