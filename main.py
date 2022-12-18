a1a34f0c94942f4ce35dda6d71725b2cd7d2d97c
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-19 02:03:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import pygame
-import pymunk
+import sys
+import pygame as pg
+import pymunk as pm
 from pymunk import pygame_util
 from level import *
 from birds import *
 from pigs import *
 from obstructions import *
 
+pymunk.pygame_util.positive_y_is_up = False
+
 
 class Game:
-
     WIDTH = 1200
     HEIGHT = 600
     FPS = 60
 
     def __init__(self):
         self.display = pg.display
-        self.sc = pg.display.set_mode((Game.WIDTH, Game.HEIGHT))
+        self.sc = self.display.set_mode((Game.WIDTH, Game.HEIGHT))
         self.icon = pg.image.load("Sprites\\cat dgap.ico")
-        pg.display.set_icon(self.icon)
+        self.display.set_icon(self.icon)
         self.clock = pg.time.Clock()
         self.draw_options = pygame_util.DrawOptions(self.sc)
         self.space = pymunk.Space()
@@ -25,30 +27,54 @@ class Game:
 
         self.game_state = -1
         self.level = Level(self.space, self.sc)
-        self.windows = [self.level_menu] + self.level.levels + [self.opening_menu]
 
         self.display.set_caption("Phystech.Birds")
 
         self.space.add_collision_handler(0, 1).post_solve = self.post_solve_bird_pig
         self.space.add_collision_handler(0, 2).post_solve = self.post_solve_bird_beam
         self.space.add_collision_handler(1, 2).post_solve = self.post_solve_pig_beam
-        self.space.add_collision_handler(0, 3).post_solve = self.post_solve_pig_ground
-        self.space.add_collision_handler(1, 3).post_solve = self.post_solve_pig_ground
-        self.space.add_collision_handler(2, 3).post_solve = self.post_solve_beam_ground
+        self.space.add_collision_handler(0, 3).post_solve = self.post_solve_bird_ground
+        self.space.add_collision_handler(0, 0).post_solve = self.post_solve_bird_bird
+        self.space.add_collision_handler(1, 1).post_solve = self.post_solve_pig_pig
+        self.space.add_collision_handler(2, 2).post_solve = self.post_solve_beam_beam
+
+        self.start_level = [self.start_level_1, self.start_level_2, self.start_level_3, self.start_level_4,
+                            self.start_level_5]
 
         pg.init()
 
+    def __call__(self, *args, **kwargs):
+        return self.gameloop()
+
+    def mark(self):
+        if self.level.score + 100 * len(self.level.birds) <= 0.3 * self.level.max_score:
+            return "УД"
+        elif 0.3 * self.level.max_score < self.level.score + 100 * len(self.level.birds) <= 0.8 * self.level.max_score:
+            return "ХОР"
+        else:
+            return "ОТЛ"
+
     def opening_menu(self):
         self.sc.fill((255, 255, 255))
-        pygame.display.update()
 
     def level_menu(self):
         self.sc.fill((255, 0, 255))
         self.sc.blit(pg.image.load("Sprites\\bg for menu 1200x600.png"), (0, 0))
-        pygame.display.update()
 
-    def __call__(self, *args, **kwargs):
-        return self.gameloop()
+    def lost(self):
+        img = pg.image.load("Sprites\\Kold.png")
+        font = pg.font.Font(None, 48)
+        lose_text = font.render('ПЕРЕСДАЧА!!!', True, (0, 0, 255))
+        self.sc.blit(img, (Game.WIDTH / 2 - img.get_size()[0] / 2, Game.HEIGHT / 2 - img.get_size()[1] / 2))
+        self.sc.blit(lose_text, (Game.WIDTH / 2 - img.get_size()[0] / 2, Game.HEIGHT / 2 + img.get_size()[1] / 2))
+
+    def win(self):
+        img = pg.image.load("Sprites\\Kold.png")
+        font = pg.font.Font(None, 48)
+        win_text = font.render(self.mark() + str(self.level.score), True, (0, 0, 255))
+        self.sc.blit(img, (Game.WIDTH / 2 - img.get_size()[0] / 2, Game.HEIGHT / 2 - img.get_size()[1] / 2))
+        self.sc.blit(win_text,
+                     (Game.WIDTH / 2 - img.get_size()[0] / 2 + 40, Game.HEIGHT / 2 + img.get_size()[1] / 2))
 
     def gameloop(self):
         finished = False
@@ -57,7 +83,7 @@ class Game:
             self.space.step(1 / self.FPS)
             for event in pg.event.get():
                 if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-                    finished = not finished
+                    finished = True
 
                 if self.game_state > 0:
                     if self.mouse_pressed(event) and self.level.number_of_birds > 0 or not self.level.mouse_is_up:
@@ -69,53 +95,84 @@ class Game:
                         self.level.mouse_is_up = True
                         self.shot()
 
-                if event.type == pygame.KEYDOWN:
-                    if self.game_state == -1 and event.key == pygame.K_RETURN:
+                if event.type == pg.KEYDOWN:
+                    if self.game_state == -1 and event.key == pg.K_RETURN:
                         self.game_state = 0
-                    elif self.game_state == 0 and event.key in [pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4,
-                                                                pygame.K_5]:
-                        keys = {pygame.K_1: 1, pygame.K_2: 2, pygame.K_3: 3, pygame.K_4: 4, pygame.K_5: 5}
+                    elif self.game_state == 0 and event.key in [pg.K_1, pg.K_2, pg.K_3, pg.K_4,
+                                                                pg.K_5]:
+                        keys = {pg.K_1: 1, pg.K_2: 2, pg.K_3: 3, pg.K_4: 4, pg.K_5: 5}
                         self.game_state = keys[event.key]
-                    elif self.game_state == 0 and event.key == pygame.K_BACKSPACE:
+                        self.start_level[self.game_state-1]()
+
+                    elif self.game_state == 0 and event.key == pg.K_BACKSPACE:
                         self.game_state = -1
-                    elif self.game_state > 0 and event.key == pygame.K_BACKSPACE:
+                    elif self.game_state > 0 and event.key == pg.K_BACKSPACE:
                         self.game_state = 0
 
-            self.windows[self.game_state]()
-            print(self.game_state, len(self.space.bodies))
-
-            if self.game_state > 0:
-                self.bird_checker()
-                # self.remover()
+            if self.game_state == -1:
+                self.opening_menu()
+            elif self.game_state == 0:
+                self.level_menu()
+            else:
+                self.re_calculator()
                 self.drawer()
+                if len(self.level.birds) == 0 and len(self.level.pigs) != 0:
+                    self.lost()
+                if len(self.level.birds) >= 0 and len(self.level.pigs) == 0:
+                    self.win()
+            self.display.update()
 
-        pg.quit()
+        sys.exit()
+
+    def start_level_1(self):
+        self.level.level1()
+
+    def start_level_2(self):
+        self.level.level2()
+
+    def start_level_3(self):
+        self.level.level3()
+
+    def start_level_4(self):
+        self.level.level4()
+
+    def start_level_5(self):
+        self.level.level5()
 
     def drawer(self):
+
         self.sc.blit(self.level.background_surf, self.level.background_surf.get_rect(bottomright=(1200, 600)))
         self.sc.blit(self.level.ground_surf, self.level.ground_surf.get_rect(bottomright=(1200, 600)))
         text_score = pg.font.Font(None, 36)
-        score = text_score.render(('SCORE: ' + str(self.level.score)), True, (255, 255, 0))
+        score = text_score.render(('SCORE: ' + str(self.level.score)), True, (255, 0, 0))
         self.sc.blit(score, (100, 50))
         for bird in self.level.birds:
             bird.draw()
         for pig in self.level.pigs:
             pig.draw()
-        for beam in self.level.obstructions:
+        for beam in self.level.beams:
             beam.draw()
         self.level.sling.draw()
-        self.display.update()
 
-    def bird_checker(self):
+
+    def re_calculator(self):
         for bird in self.level.birds:
-            bird.recalculate_calm_res()
-            if not bird.calm_res:
-                self.level.birds_to_remove.append(bird)
+            if not bird.recalculate_state():
+                self.level.birds.remove(bird)
+        for pig in self.level.pigs:
+            if not pig.recalculate_state():
+                self.level.pigs.remove(pig)
+                self.level.score += pig.cost
+        for beam in self.level.beams:
+            if not beam.recalculate_state():
+                self.level.beams.remove(beam)
+                self.level.score += beam.cost
 
     def remover(self):
         for bird in self.level.birds_to_remove:
             bird.remove()
             self.level.birds.remove(bird)
+
         for pig in self.level.pigs_to_remove:
             pig.remove()
             self.level.pigs.remove(pig)
@@ -125,8 +182,8 @@ class Game:
 
     def mouse_pressed2(self, event):
         x_mouse, y_mouse = pg.mouse.get_pos()
-        cond1 = x_mouse >= 262
-        cond2 = y_mouse <= 350
+        cond1 = x_mouse >= 235
+        cond2 = y_mouse <= 397
         cond3 = (event.type == pg.MOUSEBUTTONDOWN and event.button == 1)
         return cond1 * cond2 * cond3
 
@@ -136,7 +193,6 @@ class Game:
         cond2 = x_mouse < 262
         cond3 = y_mouse > 350
         cond4 = y_mouse < 562
-        # cond5 = pg.mouse.get_pressed()[0]
         cond5 = (event.type == pg.MOUSEBUTTONDOWN and event.button == 1)
         return cond1 * cond2 * cond3 * cond4 * cond5
 
@@ -154,100 +210,92 @@ class Game:
         unit = direction / mouse_distance
         bird_distance = min(mouse_distance, sling_length)
         bird_position = sling1 - bird_distance * unit
-        bird = self.level.birds[self.level.number_of_birds-1]
+        bird = self.level.birds[self.level.number_of_birds]
         bird.body.position = bird_position
         self.level.sling.sling_end = bird_position - unit * bird.size
 
     def shot(self):
-        bird = self.level.birds[self.level.number_of_birds-1]
-        velocity = self.level.sling.direction * 5
+        bird = self.level.birds[self.level.number_of_birds]
+        velocity = self.level.sling.direction * 8
 
         self.level.sling.reset()
         bird.body.position = self.level.sling.sling_end
         bird.launch(velocity)
         self.level.flying_bird = bird
 
-    #     обработка у каждого объекта состояния, типа нужно удалять или нет - бред
-    #     проще всего все основные решения принимать во время коллизий
-    #     сейчас реализуется удаление путём списков. Что делать с удалением медленных птичек - хз
-    #     если бы мы делали обработку состояния, то не нужно было бы условий life<=0 и т.п.
-    #     если делать без обработки, но и без списков, то после условий сразу стояли бы remove
-
     def post_solve_bird_pig(self, arbiter, space, _):
-        # здесь нужно затем продумать, сколько хпшек отнимать в зависимости от силы выстрела
-        # и в зависимости от типа объекта. Это тривиальная задача, поэтому до тестировки не прописана
-
-        ev_bird, ev_pig = arbiter.shapes
+        ev_bird_shape, ev_pig_shape = arbiter.shapes
         for pig in self.level.pigs:
-            if pig.body == ev_pig.body:
-                pig.life -= 20
-                if pig.life <= 0 and pig not in self.level.pigs_to_remove:
-                    self.level.pigs_to_remove.append(pig)
-                    self.level.score += pig.cost
+            if pig.body == ev_pig_shape.body:
+                pig.life -= 10
         for bird in self.level.birds[self.level.number_of_birds:]:
-            if bird.body == ev_bird.body:
-                bird.life -= 20
+            if bird.body == ev_bird_shape.body:
+                bird.life -= 1
                 bird.is_flying = False
-                if bird.life <= 0 and bird not in self.level.birds_to_remove:
-                    self.level.birds_to_remove.append(bird)
 
     def post_solve_bird_beam(self, arbiter, space, _):
-        ev_bird, ev_beam = arbiter.shapes
-        for beam in self.level.beams:
-            if beam.body == ev_beam.body:
-                beam.life -= 20
-                if beam.life <= 0:
-                    self.level.beams_to_remove.append(beam)
-                    self.level.score += beam.cost
-        for bird in self.level.birds[self.level.number_of_birds:]:
-            if bird.body == ev_bird.body:
-                bird.life -= 20
-                bird.is_flying = False
-                if bird.life <= 0 and bird not in self.level.birds_to_remove:
-                    self.level.birds_to_remove.append(bird)
+        if arbiter.total_impulse.length > 700:
+            ev_bird_shape, ev_beam_shape = arbiter.shapes
+            for beam in self.level.beams:
+                if beam.body == ev_beam_shape.body:
+                    beam.life -= 1
+            for bird in self.level.birds[self.level.number_of_birds:]:
+                if bird.body == ev_bird_shape.body:
+                    bird.life -= 1
+                    bird.is_flying = False
 
     def post_solve_pig_beam(self, arbiter, space, _):
-        ev_pig, ev_beam = arbiter.shapes
-        for beam in self.level.beams:
-            if beam.body == ev_beam.body:
-                beam.life -= 20
-                if beam.life <= 0 and beam not in self.level.beams_to_remove:
-                    self.level.beams_to_remove.append(beam)
-        for pig in self.level.pigs:
-            if pig.body == ev_pig.body:
-                pig.life -= 20
-            if pig.life <= 0 and pig not in self.level.pigs_to_remove:
-                self.level.pigs_to_remove.append(pig)
+        if arbiter.total_impulse.length > 700:
+            ev_pig_shape, ev_beam_shape = arbiter.shapes
+            for beam in self.level.beams:
+                if beam.body == ev_beam_shape.body:
+                    beam.life -= 1
+            for pig in self.level.pigs:
+                if pig.body == ev_beam_shape.body:
+                    pig.life -= 10
 
     def post_solve_bird_ground(self, arbiter, space, _):
-        ev_bird, ev_ground = arbiter.shapes
+        ev_bird_shape, ev_ground_shape = arbiter.shapes
 
-        if arbiter.total_impulse.length > 1000 or ev_bird.is_flying:
-            for bird in self.level.birds:
-                if bird.body == ev_bird.body:
-                    if arbiter.total_impulse_length > 1000:
-                        bird.life -= 5
-                    bird.is_flying = False
-                    if bird.life <= 0 and bird not in self.level.birds_to_remove:
-                        self.level.birds_to_remove.append(bird)
+        for bird in self.level.birds:
+            if bird.body == ev_bird_shape.body:
+                bird.is_flying = False
 
-    def post_solve_pig_ground(self, arbiter, space, _):
-        ev_pig, ev_ground = arbiter.shapes
-        if arbiter.total_impulse.length > 1000:
-            for pig in self.level.pigs:
-                if pig.body == ev_pig.body:
-                    pig.life -= 5
-                    if pig.life <= 0 and pig not in self.level.pigs_to_remove:
-                        self.level.pigs_to_remove.append(pig)
+    def post_solve_bird_bird(self, arbiter, space, _):
+        ev_bird_shape1, ev_bird_shape2 = arbiter.shapes
+        for bird in self.level.birds:
+            if bird.body == ev_bird_shape1.body:
+                ev_bird1 = bird
+                bird.is_flying = False
+            if bird.body == ev_bird_shape2.body:
+                ev_bird2 = bird
+                bird.is_flying = False
+        if arbiter.total_impulse.length > 500:
+            ev_bird1.life -= 1
+            ev_bird2.life -= 1
 
-    def post_solve_beam_ground(self, arbiter, space, _):
-        ev_beam, ev_ground = arbiter.shapes
-        if arbiter.total_impulse.length > 1000:
-            for beam in self.level.beams:
-                if beam.body == ev_beam.body:
-                    beam.life -= 5
-                    if beam.life <= 0 and beam not in self.level.beams_to_remove:
-                        self.level.beams_to_remove.append(beam)
+    def post_solve_pig_pig(self, arbiter, space, _):
+        ev_pig_shape1, ev_pig_shape2 = arbiter.shapes
+        for pig in self.level.pigs:
+            if pig.body == ev_pig_shape1.body:
+                ev_pig1 = pig
+            if pig.body == ev_pig_shape2.body:
+                ev_pig2 = pig
+                pig.is_flying = False
+        if arbiter.total_impulse.length > 500:
+            ev_pig1.life -= 1
+            ev_pig2.life -= 1
+
+    def post_solve_beam_beam(self, arbiter, space, _):
+        ev_beam_shape1, ev_beam_shape2 = arbiter.shapes
+        for beam in self.level.beams:
+            if beam.body == ev_beam_shape1.body:
+                ev_beam1 = beam
+            if beam.body == ev_beam_shape2.body:
+                ev_beam2 = beam
+        if arbiter.total_impulse.length > 500:
+            ev_beam1.life -= 1
+            ev_beam2.life -= 1
 
 
 if __name__ == "__main__":
