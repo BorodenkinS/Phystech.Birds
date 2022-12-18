e43e5cc2d75ba0f3fb7cab0dd1924a6c573724bd
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-18 19:21:19 +0300

    Update obstructions.py

--- obstructions.py
+++ obstructions.py
@@ -7,7 +7,6 @@ pymunk.pygame_util.positive_y_is_up = False
 
 
 class Sling:
-
     position = None
     direction = None
 
@@ -35,12 +34,13 @@ class Beam:
     image = None
     width = None
     length = None
+    life = None
 
     def __init__(self, x, y, is_hor, space, screen):
         if not is_hor:
             self.image = pg.transform.rotate(self.image, 90)
             shape_options = self.shape.elasticity, self.shape.friction, self.shape.collision_type
-            self.shape = pm.Segment(self.body, (0, 0), (0, self.length), self.width)
+            self.shape = pm.Poly.create_box(self.body, (self.width, self.length))
             self.shape.elasticity, self.shape.friction, self.shape.collision_type = shape_options
             self.lenx, self.leny = self.width, self.length
         else:
@@ -52,12 +52,24 @@ class Beam:
         self.space = space
 
     def draw(self):
-        angle_degrees = math.degrees(self.body.angle)
-        # self.image = pg.transform.rotate(self.image, angle_degrees)
-        self.sc.blit(self.image, self.body.position - 0.5 * pm.Vec2d(self.lenx, self.leny))
+        angle = math.degrees(self.body.angle)
+        rot_image = pg.transform.rotate(self.image, angle)
+        self.sc.blit(rot_image, self.body.position - 0.5 * pm.Vec2d(self.lenx, self.leny))
 
     def remove(self):
         self.space.remove(self.body, self.shape)
+
+    # def velocity_checker(self):
+    #     return abs(self.body.velocity) > 0.1 and abs(self.body.angular_velocity) > 0.1
+    #
+    # def recalculate_state(self):
+    #     if not self.velocity_checker():
+    #         self.body.velocity = pm.Vec2d(0, 0)
+    #         self.body.angular_velocity = 0
+    #     life_factor = self.life > 0
+    #     if not life_factor:
+    #         self.remove()
+    #     return life_factor
 
 
 class WoodBeam(Beam):
@@ -65,13 +77,14 @@ class WoodBeam(Beam):
     life = 1
     length = 100
     width = 10
+    cost = 500
     moment = mass * (width ** 2 + length ** 2) / 12
 
     def __init__(self, x, y, is_hor, space, screen):
         self.body = pm.Body(self.mass, self.moment)
-        self.shape = pm.Segment(self.body, (0, 0), (self.length, 0), self.width)
-        self.shape.elasticity = 1
-        self.shape.friction = 1
+        self.shape = pm.Poly.create_box(self.body, (self.length, self.width))
+        self.shape.elasticity = 0.1
+        self.shape.friction = 3
         self.shape.collision_type = 2
         self.image = pg.image.load("Sprites\\wood.png").convert_alpha()
         super().__init__(x, y, is_hor, space, screen)
@@ -81,14 +94,15 @@ class GlassBeam(Beam):
     mass = 1
     life = 1
     width = 10
+    cost = 100
     length = 100
     moment = mass * (width ** 2 + length ** 2) / 12
 
     def __init__(self, x, y, is_hor, space, screen):
         self.body = pm.Body(self.mass, self.moment)
-        self.shape = pm.Segment(self.body, (0, 0), (self.length, 0), self.width)
-        self.shape.elasticity = 1
-        self.shape.friction = 1
+        self.shape = pm.Poly.create_box(self.body, (self.length, self.width))
+        self.shape.elasticity = 0.1
+        self.shape.friction = 3
         self.shape.collision_type = 2
-        self.image = pg.image.load("Sprites\\glass.png").convert_alpha()
+        self.image = pg.image.load("Sprites\\wood.png").convert_alpha()
         super().__init__(x, y, is_hor, space, screen)
