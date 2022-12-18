3192d038c9d2b7e73f47462c8ac323795122fc2d
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-18 03:50:41 +0300

    Update level.py

--- level.py
+++ level.py
@@ -1,42 +1,42 @@
-import pymunk
-import pygame
+import pymunk as pm
+import pygame as pg
 from birds import *
 from pigs import *
 from obstructions import *
+from beams import *
 
 
 class Level:
-
+    # убрать атрибуты, которые заполняются в level_i
     def __init__(self, space, screen):
-        self.sc = screen
-        self.space = space
-
         self.birds = []
         self.pigs = []
+        self.beams = []
         self.obstructions = []
-        self.sling = Sling(self.sc)
-        self.sling.position = (120, 510)
-
         self.number_of_birds = 0
-
         self.mouse_is_up = True
-
         self.pigs_to_remove = []
         self.birds_to_remove = []
         self.beams_to_remove = []
-
+        self.flying_bird = None
         self.score = 0
-
+        self.sc = screen
+        self.space = space
         self.levels = [self.level1, self.level2, self.level3, self.level4, self.level5]
+        self.sling = Sling(self.sc)
+        self.sling.position = (120, 400)
 
     def level1(self):
         # проработать механизм удаления прошлых объектов.
         # просто заносить в список на удаление, наверное, недостаточно, ибо функция удаления сработает позже
         # создания новых объектов
-        background_surf = pygame.image.load('Sprites\\bg 1 1200x600.png')
-        ground_surf = pygame.image.load('Sprites\\grass.png')
-        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200,600)))
-        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+        self.background_surf = pg.image.load('Sprites\\bg 1 1200x600.png')
+        self.ground_surf = pg.image.load('Sprites\\grass.png')
+        self.ground_shape = pm.Segment(self.space.static_body, (0,578), (1200, 578), 44)
+        self.space.add(self.ground_shape)
+        self.ground_shape.friction = 1
+        self.ground_shape.elasticity = 1
+        self.ground_shape.collision_type = 3
 
         self.obstructions = [WoodBeam(1050, 475, False, self.space, self.sc),
                              WoodBeam(1000, 475, False, self.space, self.sc),
@@ -47,8 +47,6 @@ class Level:
                              WoodBeam(720, 475, False, self.space, self.sc),
                              WoodBeam(670, 475, False, self.space, self.sc),
                              WoodBeam(695, 420, True, self.space, self.sc)]
-        for beam in self.obstructions:
-            beam.draw()
 
         self.pigs = [DefaultPig(1025, 400, self.space, self.sc),
                      DefaultPig(895, 400, self.space, self.sc),
@@ -61,10 +59,13 @@ class Level:
         self.number_of_birds = 4
 
     def level2(self):
-        background_surf = pygame.image.load('Sprites\\bg 2 1200x600.png')
-        ground_surf = pygame.image.load('Sprites\\grass.png')
-        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
-        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+        self.background_surf = pg.image.load('Sprites\\bg 2 1200x600.png')
+        self.ground_surf = pg.image.load('Sprites\\grass.png')
+        self.ground_shape = pm.Segment(self.space.static_body, (0, 578), (1200, 578), 44)
+        self.space.add(self.ground_shape)
+        self.ground_shape.friction = 1
+        self.ground_shape.elasticity = 1
+        self.ground_shape.collision_type = 3
 
         self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
                              WoodBeam(995, 475, False, self.space, self.sc),
@@ -87,10 +88,13 @@ class Level:
         self.number_of_birds = 4
 
     def level3(self):
-        background_surf = pygame.image.load('Sprites\\bg 3 1200x600.jpg')
-        ground_surf = pygame.image.load('Sprites\\grass.png')
-        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
-        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+        self.background_surf = pg.image.load('Sprites\\bg 3 1200x600.jpg')
+        self.ground_surf = pg.image.load('Sprites\\grass.png')
+        self.ground_shape = pm.Segment(self.space.static_body, (0, 578), (1200, 578), 44)
+        self.space.add(self.ground_shape)
+        self.ground_shape.friction = 1
+        self.ground_shape.elasticity = 1
+        self.ground_shape.collision_type = 3
 
         self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
                              WoodBeam(995, 475, False, self.space, self.sc),
@@ -125,11 +129,13 @@ class Level:
         self.number_of_birds = 3
 
     def level4(self):
-        background_surf = pygame.image.load('Sprites\\bg 4 1200x600.jpg')
-        ground_surf = pygame.image.load('Sprites\\grass.png')
-        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
-        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
-
+        self.background_surf = pg.image.load('Sprites\\bg 4 1200x600.jpg')
+        self.ground_surf = pg.image.load('Sprites\\grass.png')
+        self.ground_shape = pm.Segment(self.space.static_body, (0, 578), (1200, 578), 44)
+        self.space.add(self.ground_shape)
+        self.ground_shape.friction = 1
+        self.ground_shape.elasticity = 1
+        self.ground_shape.collision_type = 3
         self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
                              WoodBeam(995, 475, False, self.space, self.sc),
                              WoodBeam(950, 420, True, self.space, self.sc),
@@ -149,7 +155,8 @@ class Level:
                              WoodBeam(1110, 365, False, self.space, self.sc),
                              WoodBeam(1065, 310, True, self.space, self.sc),
                              GlassBeam(1095, 365, False, self.space, self.sc),
-                             GlassBeam(1035, 365, False, self.space, self.sc)]
+                             GlassBeam(1035, 365, False, self.space, self.sc),
+                             ]
 
         self.pigs = [DefaultPig(950, 400, self.space, self.sc),
                      DefaultPig(950, 510, self.space, self.sc),
@@ -162,10 +169,13 @@ class Level:
         self.number_of_birds = 3
 
     def level5(self):
-        background_surf = pygame.image.load('Sprites\\bg 5 1200x600.jpg')
-        ground_surf = pygame.image.load('Sprites\\grass.png')
-        self.sc.blit(background_surf, background_surf.get_rect(bottomright=(1200, 600)))
-        self.sc.blit(ground_surf, ground_surf.get_rect(bottomright=(1200, 600)))
+        self.background_surf = pg.image.load('Sprites\\bg 5 1200x600.jpg')
+        self.ground_surf = pg.image.load('Sprites\\grass.png')
+        self.ground_shape = pm.Segment(self.space.static_body, (0, 578), (1200, 578), 44)
+        self.space.add(self.ground_shape)
+        self.ground_shape.friction = 1
+        self.ground_shape.elasticity = 1
+        self.ground_shape.collision_type = 3
 
         self.obstructions = [WoodBeam(905, 475, False, self.space, self.sc),
                              WoodBeam(995, 475, False, self.space, self.sc),
@@ -181,7 +191,6 @@ class Level:
                              WoodBeam(995, 255, False, self.space, self.sc),
                              GlassBeam(950, 200, True, self.space, self.sc),
                              ]
-
         self.pigs = [DefaultPig(950, 400, self.space, self.sc),
                      DefaultPig(950, 510, self.space, self.sc),
                      DefaultPig(950, 290, self.space, self.sc),
