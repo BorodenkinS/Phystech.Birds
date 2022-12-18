74f3b93c094d51fcc397748f54a0bd4bb161ea49
intex2dx <112858282+intex2dx@users.noreply.github.com>
2022-12-18 23:02:37 +0300

    Update pigs.py

--- pigs.py
+++ pigs.py
@@ -24,17 +24,18 @@ class Pig:
     def remove(self):
         self.space.remove(self.body, self.shape)
 
-    # def velocity_checker(self):
-    #     return abs(self.body.velocity) > 0.1 and abs(self.body.angular_velocity) > 0.1
-    #
-    # def recalculate_state(self):
-    #     if not self.velocity_checker():
-    #         self.body.velocity = pm.Vec2d(0, 0)
-    #         self.body.angular_velocity = 0
-    #     life_factor = self.life > 0
-    #     if not life_factor:
-    #         self.remove()
-    #     return life_factor
+    def velocity_checker(self):
+        return abs(self.body.velocity) > 0.1 or abs(self.body.angular_velocity) > 0.1
+
+    def recalculate_state(self):
+        if not self.velocity_checker():
+            self.body.velocity = pm.Vec2d(0, 0)
+            self.body.angular_velocity = 0
+            self.body.angle = 0
+        life_factor = self.life > 0
+        if not life_factor:
+            self.remove()
+        return life_factor
 
 
 class DefaultPig(Pig):
@@ -55,26 +56,26 @@ class DefaultPig(Pig):
 
     def __str__(self):
         return f"Def pos={self.body.position}"
-    
 
-class DefaultPig2(Pig):
-    mass = 5
-    life = 20
-    size = 15
-    cost = 1000
+
+class KingPig(Pig):
+    mass = 10
+    life = 40
+    size = 30
     moment = pm.moment_for_circle(mass, 0, size)
+    cost = 5000
 
     def __init__(self, x, y, space, screen):
         self.body = pm.Body(self.mass, self.moment)
         self.shape = pm.Circle(self.body, self.size, (0, 0))
-        self.shape.elasticity = 0.95
-        self.shape.friction = 1
+        self.shape.elasticity = 0.4
+        self.shape.friction = 2
         self.shape.collision_type = 1
-        self.image = pg.image.load("Sprites\\andrew.png").convert_alpha()
+        self.image = pg.image.load("Sprites\\smgshnic.png").convert_alpha()
         super().__init__(x, y, space, screen)
 
     def __str__(self):
-        return f"Def2 pos={self.body.position}"
+        return f"King pos={self.body.position}"
 
 
 class LittlePig(Pig):
@@ -97,21 +98,21 @@ class LittlePig(Pig):
         return f"Lit pos={self.body.position}"
 
 
-class KingPig(Pig):
-    mass = 10
-    life = 40
-    size = 30
+class DefaultPig2(Pig):
+    mass = 5
+    life = 20
+    size = 15
+    cost = 1000
     moment = pm.moment_for_circle(mass, 0, size)
-    cost = 5000
 
     def __init__(self, x, y, space, screen):
         self.body = pm.Body(self.mass, self.moment)
         self.shape = pm.Circle(self.body, self.size, (0, 0))
-        self.shape.elasticity = 0.4
-        self.shape.friction = 2
+        self.shape.elasticity = 0.95
+        self.shape.friction = 1
         self.shape.collision_type = 1
-        self.image = pg.image.load("Sprites\\smgshnic.png").convert_alpha()
+        self.image = pg.image.load("Sprites\\andrew.png").convert_alpha()
         super().__init__(x, y, space, screen)
 
     def __str__(self):
-        return f"King pos={self.body.position}"
+        return f"Def2 pos={self.body.position}"
